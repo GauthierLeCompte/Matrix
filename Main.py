95587eff2a719c57c335c271cfe749da0033bc8c
NickWils98 <nick.wils@student.uantwerpen.be>
2021-05-05 13:12:53 +0200

    json

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import json
+
+f = open('sequenties.json', )
+
+data = json.load(f)
+species = [[]]
+for i in data['species']:
+    specie = [i.name, i.genome]
+    species.append(specie)
+f.close()
+
+for i in species:
+    for j in species:
+        if i == j:
+            break
